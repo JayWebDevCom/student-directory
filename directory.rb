b5c51e28bb76b76bbb97e1d1b140ae78157fc642
JayWebDevCom <jaiye.s@gmail.com>
2017-03-27 15:22:06 +0100

    Add students cohort and array becomes 2d

--- directory.rb
+++ directory.rb
@@ -1,43 +1,42 @@
 # create an array of students
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  ["Dr. Hannibal Lecter", :november],
+  ["Darth Vader", :november],
+  ["Nurse Ratched", :november],
+  ["Michael Corleone", :november],
+  ["Alex DeLarge", :november],
+  ["The Wicked Witch of the West", :november],
+  ["Terminator", :november],
+  ["Freddy Krueger", :november],
+  ["The Joker", :november],
+  ["Joffrey Baratheon", :november],
+  ["Norman Bates", :november]
 ]
 
 # print a header
 def print_header
-  puts "The student of Villains Academy"
+  puts "The Students of Villains Academy"
   puts "----------"
 end
 
 # print the students array elements
-def print(names)
+def print(students)
 =begin # alternative code
-  for name in names do
-    puts names
+  for student in students do
+    puts "#{student[0]} (#{student[1]} cohort)"
   end
 =end
-  names.each do |name|
-    puts name
+  students.each do |student|
+    puts "#{student[0]} (#{student[1]} cohort)"
   end
 end
 
+# finally we print the total in the footer
 def print_footer(names)
   puts "Overall we have #{names.count} great students"
 end
 
-# finally we print the total
-
-
+# call the methods
 print_header
 print(students)
 print_footer(students)
