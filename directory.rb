2295d9b0e3b65c6d57cd04b12a6a7be8986d356a
JayWebDevCom <jaiye.s@gmail.com>
2017-03-27 19:59:35 +0100

    Add exercise 8:7

--- directory.rb
+++ directory.rb
@@ -22,69 +22,69 @@ end
 # print the students array elements
 def print(students)
 
-# define some Procs
-letter = "T"
-specific_letter = Proc.new { |student| student[:name].chr == letter }
+  # define some Procs
+  letter = "T"
+  specific_letter = Proc.new { |student| student[:name].chr == letter }
 
-name_length = 12
-short_name = Proc.new { |student| student[:name].length <= name_length }
+  name_length = 12
+  short_name = Proc.new { |student| student[:name].length <= name_length }
 
-# use a while loop or until loop
-count = 0
-while count != students.length do
-  puts "#{count+1} #{students[count][:name]} #{(students[count][:cohort])} cohort, hobby is #{(students[count][:hobby])}.".center(100)
-  count += 1
+  # use a while loop or until loop
+  count = 0
+  while count != students.length do
+    puts "#{count+1} #{students[count][:name]} #{(students[count][:cohort])} cohort, hobby is #{(students[count][:hobby])}.".center(100)
+    count += 1
+  end
+
 end
 
+def get_info(string)
+  puts "Please enter the #{string} of the student"
+  info = gets.chomp
+  if string == 'cohort'
+  months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+    info = months[rand(11)] if info.empty?
+  elsif string == 'hobby'
+    info = "*unavailable*" if info.empty?
+  end
+  info
 end
 
 # finally we print the total in the footer
 def print_footer(names)
   puts "Overall we have #{names.count} great students.".center(100)
+  p names
 end
 
 def input_students
-  puts "To finish, just hit RETURN twice"
+  puts "To finish, just hit RETURN three times"
 
   #create an empty array
   students = []
 
-  def get_name
-    puts "Please enter the name of a student"
-    name = gets.chomp
-    #get_name if name.empty?
-  end
-
-  def get_cohort
-    puts "Please enter the student cohort"
-    cohort = gets.chomp
-    #get_cohort if cohort.empty?
-  end
-
-  def get_hobby
-    puts "Please enter the student hobby"
-    hobby = gets.chomp
-    #get_hobby if hobby.empty?
-  end
-
   # get the name
-  name = get_name
-  # get the cohort
-  cohort = get_cohort
-  # get hobby info
-  hobby = get_hobby
+  name = get_info('name')
 
-  while !name.empty? && !cohort.empty? && !hobby.empty? do
+  # get the cohort
+  cohort = get_info('cohort')
+
+  #get the hobby
+  hobby = get_info('hobby')
+
+
+  while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: cohort, hobby: hobby}
+    students << {name: name, cohort: cohort.intern, hobby: hobby}
     puts "Now we have #{students.length} students..."
 
     # get the name
-    name = get_name
+    name = get_info('name')
+
     # get the cohort
-    cohort = get_cohort
-    # get hobby info
-    hobby = get_hobby
+    cohort = get_info('cohort')
+
+    #get the hobby
+    hobby = get_info('hobby')
   end
   students
 end
