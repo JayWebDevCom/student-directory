1fb11fa3824e8ee8053159b274b6b772e7614cf2
JayWebDevCom <jaiye.s@gmail.com>
2017-03-27 14:50:58 +0100

    Add printing in directory.rb

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print the list of students
 puts "The student of Villains Academy"
 puts "----------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,12 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+
+# finally we print the list of students
+print "Overall we have "
+
+# it is important that print does not add new line characters.
+print 11
+
+# but puts does
+puts " great students."
